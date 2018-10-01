fe57ddb42b0c558d03d6b94f89ae91196ffc941c
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2018-10-01 17:41:56 +0800

    added unit test sample using ava

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Harvest: Web Scraper
 A simple web scraper that takes a snapshot of a target website
 
+## Unit Test
+```
+npm test
+```
+
 ## Deploy
 ```
 gcloud functions deploy harvest-webscraper \
---
+++ test/test.js
@@ -0,0 +1,19 @@
+const test = require(`ava`);
+const uuid = require(`uuid`);
+const sinon = require(`sinon`);
+
+const pubSub = require(`..`).pubSub;
+const consoleLog = sinon.stub(console, 'log');
+
+test(`pubSub: should print Upd8`, async t => {
+  // Initialize mocks
+  const name = uuid.v4();
+  const event = {
+    data: Buffer.from(name).toString(`base64`)
+  };
+
+  // Call tested function and verify its behavior
+  await pubSub(event);
+  t.true(consoleLog.calledWith(`Upd8`));
+  t.true(consoleLog.calledWith(name));
+});
