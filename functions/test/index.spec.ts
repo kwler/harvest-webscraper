1dad6c4397de8450b07cc2d444fa4549756f244c
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2018-10-04 17:04:07 +0800

    unit test: basic example

---
+++ functions/test/index.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('index', () => {
+
+    it('should return hello world', () => {
+        const expected = 'test';
+        expect(expected).to.equal('test');
+    });
+    
+});
